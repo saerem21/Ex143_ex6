a06b4000bb7a79dbf8ab54d0b5bc49bac8630f07
saerem21 <nemessisdds>
2014-01-06 14:45:24 +0900

    액션시트 + 타임피커 입니다..
    page143

--- Ex143_ex6/ViewController.m
+++ Ex143_ex6/ViewController.m
@@ -9,10 +9,58 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *btn;
 
 @end
 
 @implementation ViewController
+{
+    UIActionSheet *sheet;
+    UIDatePicker *picker;
+    NSDateFormatter *formatter;
+    float height;
+}
+- (void)handleDone:(id)sender{
+    [sheet dismissWithClickedButtonIndex:0 animated:YES];
+    
+    if(nil == formatter){
+        formatter = [[NSDateFormatter alloc] init];
+        [formatter setDateFormat:@"yyyy 년 MM 월 dd 일"];
+        [formatter setLocale:[NSLocale currentLocale]];
+        
+    }
+    NSDate *date = picker.date;
+    NSString *dateStr = [formatter stringFromDate:date];
+    [self.btn setTitle:dateStr forState:UIControlStateNormal];
+}
+
+- (IBAction)chooseDate:(id)sender {
+    CGSize viewSize = self.view.bounds.size;
+    
+    if(nil == sheet){
+        sheet = [[UIActionSheet alloc] init];
+        
+        UIToolbar *toolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, viewSize.width,44)];
+        UIBarButtonItem *done = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(handleDone:)];
+        NSArray *items = [NSArray arrayWithObject:done];
+        [toolbar setItems:items];
+        
+        picker = [[UIDatePicker alloc] init];
+        picker.datePickerMode = UIDatePickerModeDateAndTime;
+        picker.frame = CGRectMake(0, toolbar.frame.size.height, viewSize.width, picker.frame.size.height);
+        
+        [sheet addSubview:toolbar];
+        [sheet addSubview:picker];
+        
+        [sheet showInView:self.view];
+        
+        height = toolbar.frame.size.height + picker.frame.size.height;
+        
+    }
+    
+    [sheet showInView:self.view];
+    sheet.frame = CGRectMake(0, viewSize.height - height, viewSize.width, height);
+}
 
 - (void)viewDidLoad
 {
